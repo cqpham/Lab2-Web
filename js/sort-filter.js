9de9d61e2635e216ab9867a31afe70bcb21e90b2
Chi <cqpham021@uottawa.ca>
2017-03-05 20:33:32 -0500

    Added filter side panel and fixes to filtering

--- js/sort-filter.js
+++ js/sort-filter.js
@@ -6,6 +6,7 @@ var $dashboard = $("#dashboard").isotope({
   }
 });
 
+/*
 // bind sort button click
 $('#sort-bar').on( 'click', 'button', function() {
   var sortByValue = $(this).attr('data-sort-by');
@@ -18,4 +19,33 @@ $('#filter-bar').on( 'click', 'button', function() {
   // use filterFn if matches value
   //filterValue = filterFns[ filterValue ] || filterValue;
   $dashboard.isotope({ filter: filterValue });
+});
+*/
+
+/*
+$('#filterByBrand').on('click', 'input[type="checkbox"]', function() {
+  if ($(this).prop("checked")) {
+    var filterValue = $( this ).attr('data-filter');
+    $dashboard.isotope({ filter: filterValue });
+  }
+});*/
+
+// filter with selects and checkboxes
+var $checkboxes = $('#filterByBrand input');
+
+$checkboxes.change( function() {
+  // map input values to an array
+  var inclusives = [];
+  // inclusive filters from checkboxes
+  $checkboxes.each( function( i, elem ) {
+    // if checkbox, use value if checked
+    if ( elem.checked ) {
+      inclusives.push( elem.value );
+    }
+  });
+
+  // combine inclusive filters
+  var filterValue = inclusives.length ? inclusives.join(', ') : '*';
+
+  $dashboard.isotope({ filter: filterValue })
 });
